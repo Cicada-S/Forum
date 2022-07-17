711e6683338b8fd6cfda734342371251eaf24a4b
Cicada <202586563@qq.com>
2022-07-17 23:09:14 +0800

    fingertip

--- pages/fingertip/fingertip.js
+++ pages/fingertip/fingertip.js
@@ -1,66 +1,15 @@
 // pages/fingertip/fingertip.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    x1: 0,
+    y1: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  GetPoint(event) {
+    console.log(event)
+    this.setData({
+      x1: event.touches[0].pageX,
+      y1: event.touches[0].pageY
+    })
   }
 })
