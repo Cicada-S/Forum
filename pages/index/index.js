a6f04b42573948252876a0f705c8f504cc6d2f16
Cicada <202586563@qq.com>
2022-07-29 23:19:41 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,38 @@
 // index.js
 Page({
-  data: {}
+  data: {
+    searchValue: '',
+    active: 0,
+    partition: [
+      {
+        id: '1',
+        text: '娱乐八卦',
+        icon: 'photo-o'
+      },
+      {
+        id: '2',
+        text: '二手市场',
+        icon: 'photo-o'
+      },
+      {
+        id: '3',
+        text: '表白墙',
+        icon: 'photo-o'
+      },
+      {
+        id: '4',
+        text: '失物招领',
+        icon: 'photo-o'
+      },
+      {
+        id: '5',
+        text: '学习交流',
+        icon: 'photo-o'
+      }
+    ]
+  },
+
+  onChange(event) {
+    console.log(event.detail.name)
+  },
 })
