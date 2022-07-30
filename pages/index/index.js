16d0da3648906263e321fb7f6cc7b6e10ee05007
Cicada <202586563@qq.com>
2022-07-30 22:45:51 +0800

    🌈 style: grid griditem

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
       {
         id: '1',
         name: 'Cicada',
-        text: '22-07-30 今天书籍分享:《带上她的眼睛》刘慈欣著',
+        // text: '22-07-30 今天书籍分享:《带上她的眼睛》刘慈欣著',
         // location: '佛山',
         releaseTime: '12小时前',
         partition: '娱乐八卦',
