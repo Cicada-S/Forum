294866aaebf12482b066b15082cb3f8186944634
Cicada <202586563@qq.com>
2022-08-03 23:50:03 +0800

    publis picker

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -3,6 +3,11 @@ Page({
   data: {
     content: '', // 文案内容
     fileList: [], // 文件列表
+    multiArray: [
+      ['娱乐八卦', '二手市场', '表白墙', '失物招领', '学习交流'], 
+      []
+    ],
+    multiIndex: [],
   },
 
   // 文件读取完成
@@ -21,6 +26,46 @@ Page({
     this.setData({
       fileList
     })
+  },
+
+  // 点击确认时触发
+  bindMultiPickerChange(event) {
+    console.log('picker发送选择改变,携带值为', event.detail.value)
+    this.setData({
+      multiIndex: event.detail.value
+    })
+  },
+
+  // picker的值发生改变时触发
+  bindMultiPickerColumnChange(event) {
+    console.log('修改的列为', event.detail.column,'，值为', event.detail.value);
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    data.multiIndex[event.detail.column] = event.detail.value;
+    if(event.detail.column === 0) {
+      switch (data.multiIndex[0]) {
+        case 0:
+          data.multiArray[1] = []
+          break
+        case 1:
+          data.multiArray[1] = ['出售', '求购']
+          break
+        case 2:
+          data.multiArray[1] = ['表白', '捞人', '出舍友']
+          break
+        case 3:
+          data.multiArray[1] = []
+          break
+        case 4:
+          data.multiArray[1] = ['求解', '提问']
+          break
+      }
+      data.multiIndex[1] = 0
+    }
+    console.log(data.multiIndex)
+    this.setData(data)
   },
 
   // 发布
