defe76e50d546a1fab15c804d6db29fe26af523e
Cicada <202586563@qq.com>
2022-08-28 22:37:41 +0800

    isAdministrator

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,7 +1,11 @@
 // pages/me/me.js
+const db = wx.cloud.database()
+const Administrator = db.collection('Administrator')
+
 Page({
   data: {
-    userInfo: {} // 用户信息
+    userInfo: {}, // 用户信息
+    isAdmin: false // 是否为管理员
   },
 
   /**
@@ -11,5 +15,16 @@ Page({
     // 获取用户信息
     let userInfo = wx.getStorageSync('currentUser')
     this.setData({ userInfo })
+
+    // 获取管理员信息
+    this.isAdministrator()
+  },
+
+  // 获取管理员信息
+  isAdministrator() {
+    Administrator.doc(this.data.userInfo._openid).get()
+    .then(res => {
+      console.log(res)
+    })
   }
 })
