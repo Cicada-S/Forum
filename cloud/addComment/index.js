9666202e6a591018a088c43355c50abdec91043e
Cicada <202586563@qq.com>
2022-08-29 22:45:02 +0800

    Send parent comments

---
+++ cloud/addComment/index.js
@@ -0,0 +1,30 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+const db = cloud.database()
+
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  console.log(event)
+  let data = {...event}
+  try {
+    let result = await db.collection('FatherComment').add({data})
+    
+    return {
+      code: 0,
+      data: result._id,
+      success: true      
+    }
+  }
+  catch(err) {
+    console.log(err)
+    console.error('transaction error')
+    return {
+      code: 1,
+      success: false
+    }
+  }
+}
--- pages/post/post.js
+++ pages/post/post.js
@@ -12,17 +12,17 @@ Page({
   data: {
     bottomLift: app.globalData.bottomLift,
     postInfo: {},
+    value: '', // 评论
     commentSum: 3, // 评论数量
-    avatar_url: '', // 当前用户头像
     commentList: [ // 评论列表
       {
         _id: 'fp1', // 主键
         post_id: '2', // 帖子Id
         _openid: '159', // 评论者ID
-        nickName: 'Ting', // 评论者昵称
-        avatarUrl: '/static/images/index/xtt.jpg', // 评论者头像
-        comment: '少年不惧岁月长', // 评论内容
-        time: '3小时前', // 评论时间
+        nick_name: 'Ting', // 评论者昵称
+        avatar_url: '/static/images/index/xtt.jpg', // 评论者头像
+        comment_details: '少年不惧岁月长', // 评论内容
+        comment_date: '3小时前', // 评论时间
         comment_identity: 1, // 是否为作者
         agree: 0, // 点赞数量
         child_comment: [ // 子评
@@ -64,9 +64,6 @@ Page({
    */
   onLoad(options) {
     this.getPostInfo(options.id)
-    // 当前用户头像
-    let { avatar_url } = wx.getStorageSync('currentUser')
-    this.setData({ avatar_url })
   },
 
   // 获取帖子详情信息
@@ -178,5 +175,37 @@ Page({
         this.setData({['postInfo.is_collect']: true})
       }
     }
+  },
+
+  // 评论
+  hairComment() {
+    console.log(this.data.value)
+    let { value, commentList, postInfo } = this.data
+    let userInfo = wx.getStorageSync('currentUser')
+
+    let data = {
+      post_id: postInfo._id,
+      _openid: userInfo._openid,
+      nick_name: userInfo.nick_name,
+      avatar_url: userInfo.avatar_url,
+      comment_date: new Date(),
+      comment_details: value,
+      comment_identity: postInfo._openid === userInfo._openid ? 0 : 1,
+      agree: 0
+    }
+    
+    wx.cloud.callFunction({
+      name: 'addComment',
+      data
+    }).then(res => {
+      data._id = res.result.data
+      data.comment_date = getdate(data.comment_date)
+      commentList.unshift(data)
+      this.setData({
+        value: '',
+        commentList
+      })
+    })
+
   }
 })
