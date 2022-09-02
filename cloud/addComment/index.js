682b67afa3dd410fcebc69436492dbed18c9f529
Cicada <202586563@qq.com>
2022-09-02 22:32:13 +0800

    Optimize the code for adding comments

--- cloud/addComment/index.js
+++ cloud/addComment/index.js
@@ -5,30 +5,24 @@ cloud.init({
 })
 const db = cloud.database()
 
-
 // 云函数入口函数
 exports.main = async (event, context) => {
-  console.log(event)
   let data = {...event}
   try {
+    let result = {}
     if(data.parent_id) {
-      let result = await db.collection('SonComment').add({data})
-      return {
-        code: 0,
-        data: result._id,
-        success: true      
-      }
+      result = await db.collection('SonComment').add({data})
     } else {
       result = await db.collection('FatherComment').add({data})
-      return {
-        code: 0,
-        data: result._id,
-        success: true      
-      }
+    }
+
+    return {
+      code: 0,
+      data: result._id,
+      success: true      
     }
   }
   catch(err) {
-    console.log(err)
     console.error('transaction error')
     return {
       code: 1,
