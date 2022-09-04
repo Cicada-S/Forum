36a21057ee76e77c30cf6a2da0c8ef480a9b4215
Cicada <202586563@qq.com>
2022-09-04 15:16:14 +0800

    myPost getPostList

--- pages/myPost/myPost.js
+++ pages/myPost/myPost.js
@@ -1,66 +1,23 @@
 // pages/myPost/myPost.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    postList: [], // 帖子列表
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 页面加载
    */
   onLoad(options) {
-
+    console.log(options)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+  // 获取帖子列表
+  async getPostList() {
+    let result = await wx.cloud.callFunction({
+      name: 'getPostList',
+      data: { id: wx.getStorageSync('currentUser')._openid }
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    console.log(result)
   }
 })
