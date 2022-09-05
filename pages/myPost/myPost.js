5ea9beeee0add0b5c66d851ac8ac737d980ccb15
Cicada <202586563@qq.com>
2022-09-05 22:03:11 +0800

    Show my post

--- pages/myPost/myPost.js
+++ pages/myPost/myPost.js
@@ -1,4 +1,7 @@
 // pages/myPost/myPost.js
+// 引入date
+import { getdate } from '../../utils/date'
+
 Page({
   data: {
     postList: [], // 帖子列表
@@ -7,17 +10,19 @@ Page({
   /**
    * 页面加载
    */
-  onLoad(options) {
-    console.log(options)
+  onLoad() {
+    // 获取帖子列表
+    this.getPostList()
   },
 
   // 获取帖子列表
   async getPostList() {
-    let result = await wx.cloud.callFunction({
+    let { result } = await wx.cloud.callFunction({
       name: 'getPostList',
       data: { id: wx.getStorageSync('currentUser')._openid }
     })
-
-    console.log(result)
+    // 将发布时间改成文字
+    result.data.forEach(item => item.publish_date = getdate(item.publish_date))
+    this.setData({ postList: result.data })
   }
 })
