caa7300858b586633243ea0baf908f4f985617a0
Cicada <202586563@qq.com>
2022-09-07 23:03:50 +0800

    Information management

---
+++ pages/swiperAdmin/swiperAdmin.js
@@ -0,0 +1,19 @@
+// pages/swiperAdmin/swiperAdmin.js
+Page({
+  data: {
+    informationList: [
+      {
+        _id: '123124fsa',
+        path: '/static/images/swiperAdmin/xtt.jpg',
+        upload_date: '22-08-21',
+        remarks: '少年不惧岁月长，彼方尚有荣光在'
+      },
+      {
+        _id: 'adadsadw',
+        path: '/static/images/swiperAdmin/xtt.jpg',
+        upload_date: '21-03-20',
+        remarks: '劝君莫惜金缕衣，劝君须取少年时'
+      }
+    ]
+  }
+})
