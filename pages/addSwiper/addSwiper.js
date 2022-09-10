fbeb21c0eac8d121accccd685da4baef17dc40bc
Cicada-S <202586563@qq.com>
2022-09-10 16:35:12 +0800

    ✌ optimize code

--- pages/addSwiper/addSwiper.js
+++ pages/addSwiper/addSwiper.js
@@ -18,32 +18,32 @@ Page({
 
   // 添加资讯
   async onClick() { 
-    const { remarks, fileList } =  this.data
     // 判断备注和图片是否为空
-    if(this.isEmpty()) {
-      // 文件名
-      const cloudPath = 'swiper/' + uuid() + fileList[0].url.match(/.[^.]+$/)[0]
-      // 将图片上传到云空间
-      const result = await wx.cloud.uploadFile({
-        cloudPath,
-        filePath: fileList[0].url
-      })
+    if(!this.isEmpty()) return
 
-      console.log('result', result)
-      Information.add({data: {
-        path: result,
-        upload_date: new Date(),
-        remarks
-      }}).then(res => {
-        console.log('res', res)
-      })
-    }
+    const { remarks, fileList } =  this.data
+    // 文件名
+    const cloudPath = 'swiper/' + uuid() + fileList[0].url.match(/.[^.]+$/)[0]
+    // 将图片上传到云空间
+    const result = await wx.cloud.uploadFile({
+      cloudPath,
+      filePath: fileList[0].url
+    })
+
+    console.log('result', result)
+    Information.add({data: {
+      path: result,
+      upload_date: new Date(),
+      remarks
+    }}).then(res => {
+      console.log('res', res)
+    })
   },
 
   // 判断备注和图片是否为空
   isEmpty() {
     const { remarks, fileList } =  this.data
-    if(!remarks) {
+    if(!remarks.trim()) {
       wx.showToast({
         title: '备注不能为空!',
         icon: 'none',
