0a22758cdec186fac445cb440e2e09916a8d9ff1
Cicada <202586563@qq.com>
2022-09-16 21:53:15 +0800

    Data modification function completed

--- pages/editProfile/editProfile.js
+++ pages/editProfile/editProfile.js
@@ -1,4 +1,7 @@
 // pages/editProfile/editProfile.js
+const db = wx.cloud.database()
+const User = db.collection('User')
+
 Page({
   data: {
     userInfo: {}
@@ -11,5 +14,22 @@ Page({
     this.setData({
       userInfo: wx.getStorageSync('currentUser')
     })
+  },
+
+  // 监听个性签名发生改变
+  onChange(event) {
+    this.setData({ 'userInfo.autograph': event.detail })
+  },
+
+  // 保存的回调函数
+  async onClick() {
+    let userInfo = this.data.userInfo
+    await User.doc(userInfo._id).update({data: {autograph: userInfo.autograph}})
+    wx.showToast({
+      title: '保存成功！',
+      icon: 'none',
+      duration: 1000
+    })
+    wx.setStorageSync('currentUser', userInfo)
   }
 })
