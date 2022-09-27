2764b874fa8d172eea06d6860bb60e3f0a04cb11
Cicada <202586563@qq.com>
2022-09-27 23:46:10 +0800

    Re perfect the page about us

--- pages/aboutUs/aboutUs.js
+++ pages/aboutUs/aboutUs.js
@@ -1,6 +1,22 @@
 // pages/aboutUs/aboutUs.js
+const db = wx.cloud.database()
+const Administrator = db.collection('Administrator')
+
 Page({
   data: {
+    authorInfo: {},
+    authorInt: '小苏同学，18届计算机专业学生',
+    introduce: '校园论坛是一个信息的港湾，它集结了许许多多的内容，为有共同爱好的同学创造了另一片交流的空间。',
+    email: '202586563@qq.com'
+  },
 
+  /**
+   * 页面加载
+   */
+  onLoad() {
+    Administrator.doc('058dfefe630cb5b2174ca4fc61630491').get()
+    .then(res => {
+      this.setData({ authorInfo: res.data })
+    })
   }
 })
