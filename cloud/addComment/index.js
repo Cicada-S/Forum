d3ac8b737ba20ca344087b3b48eaa5cbca4517ec
Cicada <202586563@qq.com>
2022-10-21 23:59:35 +0800

    Comment on security detection

--- cloud/addComment/index.js
+++ cloud/addComment/index.js
@@ -7,6 +7,21 @@ const db = cloud.database()
 exports.main = async (event, context) => {
   let { userInfo, ...data } = event
   try {
+    // 内容安全检测
+    const msgSecCheckRes = cloud.callFunction({
+      name: 'msgSecCheckRes',
+      data: { text: data.content }
+    })
+
+    // 判断文本是否违规
+    if(msgSecCheckRes.result.errcode != 0) {
+      return {
+        code: 1,
+        error: '文字內容违规',
+        success: false
+      }
+    }
+
     let result = {}
     if(data.parent_id) {
       result = await db.collection('SonComment').add({data})
